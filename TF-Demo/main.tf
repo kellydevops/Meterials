b949fdfc968a16f4bafdd2a7a5754852b77318a9
akhil <you@example.com>
2024-01-10 09:23:16 +0530

    akhil_changes

--- TF-Demo/main.tf
+++ TF-Demo/main.tf
@@ -1,42 +1,35 @@
 provider "aws" {
-   region = "us-west-2"
-   
-}
-
-resource "aws_instance" "test" {
-  ami = "ami-0ab193018f3e9351b"
-  instance_type = "t2.micro"
-  tags = {
-    name = "testec2"
-  }
-}
-
-resource "aws_s3_bucket" "demobkt" {
-    bucket = "kelytests3bkt"
+  region = var.region
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = "192.168.0.0/16"
-    tags = {
-      name = "testvpc"
-    }
+    cidr_block = var.cidr_block[0]
+
+ tags = {
+    Name = var.tags[0]
+ } 
+ 
   
 }
 
-resource "aws_subnet" "subnet1" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "192.168.0.0/24"
-    tags = {
-        name = "public sbunet"
-    }
-  
+resource "aws_subnet" "sub1" {
+   vpc_id = aws_vpc.main.id
+   cidr_block = var.cidr_block[1]
+
+   tags = {
+     Name = var.tags[1]
+   }
 }
 
-resource "aws_subnet" "subnet2" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "192.168.1.0/24"
-    tags = {
-        name = "private vpc"
-    }
-  
+resource "aws_subnet" "sub2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.cidr_block[2]
+
+  tags = {
+    Name = var.tags[2]
+  }
 }
+
+
+
+
